47929b5aab2e40a5efe2c7d8c60d8607852642dd
PlutonicOverkill <dosgamesandstuff@gmail.com>
2016-11-02 13:26:27 +1300

    Convert map scripts to C
    Convert the TITLEMAP scripts to C, which are intended for the GDCC-CC
    compiler.

---
+++ src/titlemap.h
@@ -0,0 +1,28 @@
+struct Hud_image {
+	__str font;
+	
+	int width;
+	int height;
+	
+	int msg_id;
+	
+	accum hold_time;
+	
+	accum fadein_time; // 0.0 means HUDMSG_FADEOUT, unless >
+	accum fadeout_time; // 0.0 means HUDMSG_PLAIN
+	
+	int delay;
+};
+
+[[call("ScriptS"), script("Open")]]
+void map01_open(void);
+
+void reset_player(void);
+
+[[call("ScriptS")]]
+void print_msg(void);
+
+[[call("ScriptS")]]
+void show_cutscene(void);
+
+void display_image(struct Hud_image);
