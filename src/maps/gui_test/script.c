9b2cb16cc7dc0a3727b3b838f3c5880d362c0a69
PlutonicOverkill <dosgamesandstuff@gmail.com>
2016-11-28 10:08:42 +1300

    Started GUI map script

--- src/maps/gui_test/script.c
+++ src/maps/gui_test/script.c
@@ -8,14 +8,47 @@
 #define FIRST_DOOR	16
 #define SECOND_DOOR	17
 
-[[call("ScriptS"), script("Open")]]
+#define RANGE	32.0
+
+int shooter_tid;
+int cursor_tid;
+
+[[call("ScriptS"), script("Enter")]]
 void main(void)
 {
-	ACS_SetCameraToTexture(7, s"GUI3DTEX", 90)
+	ACS_SetCameraToTexture(7, s"GUI3DTEX", 90); // init screen
+	
+	if(!ACS_ActivatorTID())
+		ACS_Thing_ChangeTID(0, ACS_UniqueTID(1, 0));
+	int player_tid = ACS_ActivatorTID(); // give player unique tid
+	
+	shooter_tid = ACS_UniqueTID(1, 0);
+	cursor_tid = ACS_UniqueTID(1, 0); // give initial values so Thing_Remove doesn't do anything dangerous
+	
+	for(;;)
+	{
+		ACS_Thing_Remove(shooter_tid);
+		ACS_Thing_Remove(cursor_tid); // get rid of existing actors
+		
+		shooter_tid = ACS_UniqueTID(1, 0);
+		ACS_SpawnSpot(s"MapSpot", player_tid, shooter_tid); // spawn hitscan shooter at player position
+		
+		cursor_tid = ACS_UniqueTID(1, 0);
+		ACS_LineAttack(shooter_tid, ACS_GetActorAngle(player_tid), ACS_GetActorPitch(player_tid), 0, s"Cursor", s"", RANGE, FHF_NORANDOMPUFFZ | FHF_NOIMPACTDECAL, cursor_tid);
+		
+		ACS_Delay(1);
+	}
 }
 
 [[call("ScriptS")]]
 void gui_activate(void)
+{
+	if(ACS_ActivatorTID() != shooter_tid)
+		return; // make sure only activated by shooter
+	
+	// do stuff with cursor_tid
+	
+}
 
 [[call("ScriptS")]]
 void open_airlock(void)
