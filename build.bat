283d5e977458fba9541f7a2ac3ce39123c81b263
PlutonicOverkill <dosgamesandstuff@gmail.com>
2017-01-07 16:54:18 +1300

    Add a build script

---
+++ build.bat
@@ -0,0 +1,31 @@
+@ECHO off
+rem assumes 7zip Extras is placed into a directory adjacent to this directory
+
+rem works for now but creates empty .git and ir directories - need to create exclude list
+
+rem change to working directory
+CD /D %~dp0
+
+rem force libc recompilation
+IF EXIST acs\libc.lib DEL acs\libc.lib
+
+CALL compile.bat
+
+IF NOT EXIST ..\7z\ (
+	ECHO 7zip must be in a directory adjacent
+	ECHO to this script for building to work!
+	
+	EXIT /B
+)
+
+rem mostly stolen from wolfendoom boa
+FOR /F %%I IN ('GIT --git-dir=".\.git" rev-parse --short HEAD') DO SET _git_commit_hash=%%I
+
+SET _build_name=Xenotomb-%_git_commit_hash%.pk3
+
+IF EXIST ..\%_build_name% DEL ..\%_build_name%
+
+
+START "Building Xenotomb..." /B /WAIT "..\7z\7za.exe" a  -r -tzip -x@".7zignore" "..\%_build_name%" ".\*"
+
+PAUSE
