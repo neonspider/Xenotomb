d4875e1420757d34d9d5ab515fe22e135441e05b
PlutonicOverkill <dosgamesandstuff@gmail.com>
2017-01-13 17:18:17 +1300

    cleanup long lines in scripts

--- src/maps/gui_test/script.c
+++ src/maps/gui_test/script.c
@@ -9,7 +9,7 @@
 enum {first_door = 16};
 enum {second_door = 17};
 
-enum {range	= 8192 << 8};
+enum {range	= 8192<<8};
 
 enum {screen_topleft = 2, screen_bottomleft = 3, screen_bottomright = 4};
 
@@ -26,7 +26,7 @@ void main(void)
 {
 	ACS_SetCameraToTexture(camera_tid, s"GUI3DTEX", 90); // init screen
 	
-	if(!ACS_ActivatorTID())
+	if (!ACS_ActivatorTID())
 		ACS_Thing_ChangeTID(0, ACS_UniqueTID(1, 0));
 	player_tid = ACS_ActivatorTID(); // give player unique tid
 	
@@ -36,14 +36,16 @@ void main(void)
 	cursor_tid = safe_tid();
 	debug_msg("cursor_tid:  %i\n", cursor_tid);
 	
-	for(;;) {
+	for (;;) {
 		ACS_Thing_Remove(shooter_tid);
 		ACS_Thing_Remove(cursor_tid); // get rid of existing actors
 		
 		ACS_SpawnForced(s"GuiShooter", ACS_GetActorX(player_tid), ACS_GetActorY(player_tid),
-			ACS_GetActorZ(player_tid) + ACS_GetActorViewHeight(player_tid) + zoffset_hack, shooter_tid); // spawn hitscan shooter at player position
+			ACS_GetActorZ(player_tid)+ACS_GetActorViewHeight(player_tid)+zoffset_hack,
+				shooter_tid); // spawn hitscan shooter at player position
 		
-		ACS_LineAttack(shooter_tid, ACS_GetActorAngle(player_tid), ACS_GetActorPitch(player_tid), 0, s"GuiCursor", s"None", range, FHF_NORANDOMPUFFZ | FHF_NOIMPACTDECAL, cursor_tid);
+		ACS_LineAttack(shooter_tid, ACS_GetActorAngle(player_tid), ACS_GetActorPitch(player_tid),
+			0, s"GuiCursor", s"None", range, FHF_NORANDOMPUFFZ|FHF_NOIMPACTDECAL, cursor_tid);
 		
 		ACS_Delay(1);
 	}
@@ -52,7 +54,7 @@ void main(void)
 [[call("ScriptS"), extern("ACS")]]
 void gui_activate(void)
 {
-	if(ACS_ActivatorTID() != shooter_tid)
+	if (ACS_ActivatorTID()!=shooter_tid)
 		return;
 	
 	
@@ -68,6 +70,6 @@ void open_airlock(void)
 void finish_airlock(void)
 {
 	ACS_Door_Close(first_door, 64, 0);
-	ACS_Delay(35 * 2);
+	ACS_Delay(35*2);
 	ACS_Door_Open(second_door, 16, 0);
 }
