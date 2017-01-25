edb32bfbba8b19c686cedf4e262d2e95b3147d95
PlutonicOverkill <dosgamesandstuff@gmail.com>
2017-01-25 15:17:40 +1300

    Debug identifier should come last

--- build.bat
+++ build.bat
@@ -13,23 +13,10 @@ IF NOT EXIST ..\7z\ (
 	EXIT /B
 )
 
-REM mostly stolen from wolfendoom boa build.bat
-REM FOR /F %%I IN ('GIT --git-dir=".\.git" rev-parse --short HEAD') DO SET _git_commit_hash=%%I
-
-
-REM FOR /F %%I IN ('GIT --git-dir=".\.git" rev-list --count HEAD') DO SET _git_commit_num=%%I
-
-REM SET "_xeno_version="
-		REM IF EXIST VERSION.txt (
-			REM FOR /F %%I IN (VERSION.txt) DO (
-				REM SET _xeno_version=%%I
-			REM )
-		REM )
-
 FOR /F %%I IN ('GIT --git-dir=".\.git" describe --long --tags --dirty --always') DO SET _git_version=%%I
 
 SET _build_name_rel=Xenotomb-%_git_version%.pk3
-SET _build_name_deb=Xenotomb-debug-%_git_version%.pk3
+SET _build_name_deb=Xenotomb-%_git_version%-debug.pk3
 IF EXIST ..\%_build_name_rel% DEL ..\%_build_name_rel%
 IF EXIST ..\%_build_name_deb% DEL ..\%_build_name_deb%
 
